d022c560f1d79108261f7ca402ec00d3ac211134
GIULIA BAX s345248 <s345248@studenti.polito.it>
2025-08-28 13:12:05 +0200

    Refactor factorial function to use saturating multiplication; update test cases with specific values and add validation to Ticket struct methods.

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -18,7 +18,19 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        if status.as_str() != "To-Do" && status.as_str() != "In Progress" && status.as_str() != "Done" {
+          panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+        if title.len() > 50 {
+          panic!("Title cannot be longer than 50 bytes")
+        } else if title.is_empty() {
+          panic!("Title cannot be empty")
+        }
+        if description.len() > 500 {
+          panic!("Description cannot be longer than 500 bytes")
+        } else if description.is_empty() {
+          panic!("Description cannot be empty")
+        }
         Self {
             title,
             description,
